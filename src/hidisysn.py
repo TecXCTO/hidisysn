0bc5ea873510f4114cf5c4d425083c4e173fea8c
TecXCTO <cto.tecx@gmail.com>
2026-01-21 14:55:33 +0530

    Import numpy and add test method to HiDiSysN
    Added numpy import and test method to HiDiSysN class.

--- src/hidisysn.py
+++ src/hidisysn.py
@@ -1,4 +1,6 @@
 
+import numpy as np
+
 class HiDiSysN:
   def __ini__(self, *args, **kwargs):
     #
@@ -6,6 +8,8 @@ class HiDiSysN:
   def train(self, edim=8, pldim=48, eddim=72):
     __init__()
     return 
+  def test(self):
+    hds
 
 if __name__==main():
   hdsn = HiDiSysN
